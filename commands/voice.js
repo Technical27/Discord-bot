da524f17ff6be4f24b721c295d1819b56a59ce0d
Aamaruvi Yogamani <aamaruvi@pop-os.localdomain>
2019-04-23 06:56:05 -0400

    Added ablity for custom voice commands

--- commands/voice.js
+++ commands/voice.js
@@ -1,8 +1,34 @@
 const ytdl = require('ytdl-core');
 const {guilds} = require('../db');
 const Discord = require('discord.js');
-const vMap = new Discord.Collection([
+const sounds = new Discord.Collection([
   ['mi', 'https://www.youtube.com/watch?v=XAYhNHhxN0A']
+]);
+
+const playSound = async (msg, link) => {
+  const vconn = msg.guild.voiceConnection;
+  if (vconn) vconn.disconnect();
+  const vc = msg.member.voiceChannel;
+  if (!vc) return msg.channel.send('You are not in any voice channel');
+
+  const {volume} = await guilds.findOne({where: {guildId: msg.guild.id}});
+  const conn = await vc.join();
+  setTimeout(() => {
+    const stream = ytdl(link, {filter: 'audioonly', highWaterMark: 33554432});
+    const dispatcher = conn.playStream(stream);
+    dispatcher.setVolume(volume/100);
+    dispatcher.once('end', () => vc.leave());
+  }, 2000);
+};
+
+const yt = (msg, args) => {
+  const link = args[1];
+  if (!ytdl.validateURL(link)) return msg.channel.send('Invalid youtube link');
+  playSound(msg, link);
+};
+
+const commands = new Discord.Collection([
+  ['youtube', yt]
 ]);
 
 module.exports = {
@@ -13,18 +39,8 @@ module.exports = {
   args: true,
   guildOnly: true,
   execute: async (msg, args) => {
-    if (!vMap.has(args[0])) return msg.channel.send(`That is not a valid sound, here is a list of some:\n\`${vMap.keyArray().join(', ')}\``);
-    const vconn = msg.guild.voiceConnection;
-    if (vconn) vconn.disconnect();
-    const vc = msg.member.voiceChannel;
-    if (vc) {
-      const {volume} = await guilds.findOne({where: {guildId: msg.guild.id}});
-      const conn = await vc.join();
-      const stream = ytdl(vMap.get(args[0]), {filter: 'audioonly'});
-      const dispatcher = conn.playStream(stream);
-      dispatcher.setVolume(volume/100);
-      dispatcher.once('end', () => vc.leave());
-    }
-    else msg.channel.send('You are not in any voice channel');
+    if (commands.has(args[0])) return commands.get(args[0])(msg, args);
+    if (!sounds.has(args[0])) return msg.channel.send(`That is not a valid sound, here is a list of some:\n\`${sounds.keyArray().join(', ')}\``);
+    playSound(msg, sounds.get(args[0]));
   }
 };
